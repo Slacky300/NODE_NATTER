cb0c9b9881f7cbb74dbb4e0c46a18fad2125731b
Rehman Khan <rehman.khan.300a@gmail.com>
2024-01-08 12:17:28 +0530

    added google analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,28 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import './App.css';
-import Login from './Pages/Auth/Login';
-import Register from './Pages/Auth/Register';
-import ChatRoom from './Pages/Main/ChatRoom';
-import AllRooms from './Pages/Main/AllRooms';
-import Layout from './Pages/Main/Layout';
-import LandingPage from './Pages/Main/LandingPage';
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Auth/Register";
+import ChatRoom from "./Pages/Main/ChatRoom";
+import AllRooms from "./Pages/Main/AllRooms";
+import Layout from "./Pages/Main/Layout";
+import LandingPage from "./Pages/Main/LandingPage";
+import ReactGA from "react-ga";
+const TRACKING_ID = "G-DVVJ3Q0KH7"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 function App() {
-
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <Layout />,
       children: [
-        { path: '/', element: <LandingPage /> },
-        { path: '/login', element: <Login /> },
-        { path: '/register', element: <Register /> },
-        { path:  '/rooms', element: <AllRooms /> },
-        { path:  '/chat/:roomId/:roomName' , element:<ChatRoom />  },
+        { path: "/", element: <LandingPage /> },
+        { path: "/login", element: <Login /> },
+        { path: "/register", element: <Register /> },
+        { path: "/rooms", element: <AllRooms /> },
+        { path: "/chat/:roomId/:roomName", element: <ChatRoom /> },
       ],
     },
   ]);
