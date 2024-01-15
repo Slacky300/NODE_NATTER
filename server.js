5ca2c76548a1386d5ccd55d42409506796e52757
Slacky300 <rehman.khan.300a@gmail.com>
2024-01-15 15:07:22 +0530

    added workflow file

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
   dbConnect();
